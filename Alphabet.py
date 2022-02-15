4d598be8f64c389501fc6dea9fd70f8524bda1fc
kevin clerima <kevin.clerima@outlook.fr>
2022-02-15 19:08:09 +0100

    1ere epreuve terre

---
+++ Alphabet.py
@@ -0,0 +1,9 @@
+#/usr/bin/python3
+# definir une fonction qui affiche l'alphabet en minuscule
+
+def alpha_minus():
+
+    alpha= "abcdefghijklmnopqrstuvwxyz\n"
+    
+    for i in alpha:
+        print(i, end='')
