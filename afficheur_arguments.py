5ecd2b7b60b3f44316e6fab596a0b6b363f63e0a
kevin <87566664+KevTon972@users.noreply.github.com>
2023-01-09 14:24:07 +0100

    Update afficheur_arguments.py

--- afficheur_arguments.py
+++ afficheur_arguments.py
@@ -1,12 +1,12 @@
+'''
+Creez un programme qui affiche les arguments qu'il reçoit ligne par ligne
+'''
 #!/usr/bin/python3
- 
-#-*- coding:utf-8 -*-
-
 import sys
 
 def afficheur():
+    for arg in sys.argv:
+        print(arg)
 
-       for arg in sys.argv:
-           print(arg)
-        
-afficheur()
+if __name__ == '__main__':
+    afficheur()
