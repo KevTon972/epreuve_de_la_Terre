3966ff00e5737fc08b38fab87130c11e3e10a64b
kevin <87566664+KevTon972@users.noreply.github.com>
2023-01-09 14:58:43 +0100

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,7 +1,21 @@
-#!/usr/bin/env python3
-
+'''
+Creez un programme qui affiche l'inverse de la
+chaine de caractères donnée.
+'''
 import sys
 
-chaine = sys.argv[1]
+def reverse_string():
+    try:
+        chaine = sys.argv[1]
 
-print(chaine[::-1])
+        if chaine.isdigit():
+            print("L'argument doit être une chaîne de caractères.")
+            return
+
+        print(chaine[::-1])
+    except:
+        print("probleme d'argument.")
+
+if __name__ == '__main__':
+    reverse_string()
+    
