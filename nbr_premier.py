8f0ea34ef33ad8af05b715894ac37beab0d7bf70
kevin <87566664+KevTon972@users.noreply.github.com>
2023-01-09 15:34:41 +0100

    Update nbr_premier.py

--- nbr_premier.py
+++ nbr_premier.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python3
-
+'''
+Creez un programme qui affiche si un nombre est premier ou pas.
+'''
 import sys
 
-arg = int(sys.argv[1])
+def nbr_premier():
+    try:
+        arg = int(sys.argv[1])
 
-if arg > 1:
+        if arg > 1:
+            for i in range(2, arg):
+                if (arg % i) == 0:
+                    print(f"Non, {arg} n’est pas un nombre premier.")
+                    return
+            else:
+                print(f"Oui, {arg} est un nombre premier.")
+        else:
+            print("le nombre doit être superieur à 1.")
+    except:
+        print("Erreur d'argument.")
+
+if __name__ == '__main__':
+    nbr_premier()
     
-    for i in range(2, arg):
- 
-              
-        if (arg % i) == 0:
-            print(f"Non, {arg} n’est pas un nombre premier.")
-            break
-    else:
-        print(f"Oui, {arg} est un nombre premier.")
- 
-else:
-    print(f"Non, {arg} n’est pas un nombre premier.")
