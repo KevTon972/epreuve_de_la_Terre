095eef0634d759884db7d64d36091c4cddbcb375
kevin <87566664+KevTon972@users.noreply.github.com>
2023-01-12 15:14:35 +0100

    Update 12_to_24.py

--- 12_to_24.py
+++ 12_to_24.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python3
-
+'''Créez un programme qui transforme une heure
+affichée en format 12h en format 24h.
+'''
 import sys
 from datetime import datetime
 
-arg = sys.argv[1]
+def hours(arg):
+    h12 = datetime.strptime(arg, "%I:%M%p")
+    h24 = h12.strftime("%H:%M")
 
-h12 = datetime.strptime(arg, "%I:%M%p")
+    print(h24)
 
-h24 = h12.strftime("%H:%M")
-
-print(h24)
+if __name__ == '__main__':
+    arg = sys.argv[1]
+    hours(arg)
+    
