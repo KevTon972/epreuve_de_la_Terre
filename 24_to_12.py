301fa627b294554e55deff726ea3342cf3349637
kevin <87566664+KevTon972@users.noreply.github.com>
2023-01-12 15:15:26 +0100

    Update 24_to_12.py

--- 24_to_12.py
+++ 24_to_12.py
@@ -4,12 +4,12 @@ affichée en format 24h en format 12h.
 from datetime import datetime
 import sys
 
-def hours():
-    arg  = sys.argv[1]
+def hours(arg):
     h24 = datetime.strptime(arg, "%H:%M")
     h12 = h24.strftime("%I:%M%p")
 
     print(h12)
 
 if __name__ == '__main__':
-    hours()
+    arg  = sys.argv[1]
+    hours(arg)
